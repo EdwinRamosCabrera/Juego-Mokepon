f2d70f73eac57ab9ac9f813a4e5b5b11114065a2
Edwin Ramos Cabrera <edwin_ramos2@usmp.pe>
2023-01-31 23:26:06 -0500

    Creación de servidor

---
+++ index.js
@@ -0,0 +1,13 @@
+// console.log('Hola Node');
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res ) => {
+    res.send('Hola');
+})
+
+app.listen(8080, () => {
+    console.log('Servidor funcionando')
+})
